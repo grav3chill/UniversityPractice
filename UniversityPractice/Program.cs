2f3c39b118ddd65125a76ec2b62f81a2e8c3fb4e
misha <mixas01102004@gmail.com>
2025-06-10 10:53:35 +0300

    Добавил функции добавления и вывода элементов в списки; вынес функции в отдельные методы, дабы не дублировать код

--- UniversityPractice/Program.cs
+++ UniversityPractice/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ListPractice
+﻿using System.Runtime.ExceptionServices;
+
+namespace ListPractice
 {
     /// <summary>
     /// Консольное интерактивное приложение, показывающее работу с коллекцией List<>
@@ -16,13 +18,54 @@
     /// </summary>
     internal class Program
     {
+        public static void DisplayOptionsForList(int choice)
+        {
+            Console.Clear();
+            Console.WriteLine($"Выбран список {choice}");
+            Console.WriteLine("Выбери операцию со списом:");
+            Console.WriteLine("1. Добавление элементов в список");
+            Console.WriteLine("2. Удаление эелементов из списка");
+            Console.WriteLine("3. Сортировка списка");
+            Console.WriteLine("4. Вставка элементов в список по индексу");
+            Console.WriteLine("5. Поиск элементов в списке");
+            Console.WriteLine("6. Копирование списка в другой список");
+            Console.WriteLine("7. Вывод элементов списка");
+        }
+
+        public static void AddElementsToList(List<object> list)
+        {
+            Console.Clear();
+            Console.WriteLine("Выбрано добавление элементов");
+            Console.WriteLine("Добавляй элементы по одному! :)");
+            Console.WriteLine("Чтобы завершить ввод нажми Enter");
+
+            while (true)
+            {
+                var elementInput = Console.ReadLine();
+                if (string.IsNullOrEmpty(elementInput))
+                    break;
+                list.Add(elementInput);
+            }
+        }
+        public static void OutputElementsOfList(List<object> list)
+        {
+            Console.Clear();
+            Console.WriteLine("Элементы списка:");
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
+        }
         static void Main(string[] args)
         {
+            var firstList = new List<object>();
+            var secondList = new List<object>();
+            int listInput = 0;
+            int optionsInput = 0;
+
             while (true) 
             { 
-                var firstList = new List<object>();
-                var secondList = new List<object>();
-                int listInput = 0;
+                
                 try
                 {
                     Console.WriteLine("Выбери список \n1. Список 1 \n2. Список 2");
@@ -35,23 +78,54 @@
                 switch (listInput)
                 {
                     case 1:
-                        Console.Clear();
-                        Console.WriteLine("Выбран список 1");
-                        Console.WriteLine("Выбери операцию со списом:");
-                        Console.WriteLine("1. Добавление элементов в список");
-                        Console.WriteLine("2. Удаление эелементов из списка");
-                        Console.WriteLine("3. Сортировка списка");
-                        Console.WriteLine("4. Вставка элементов в список по индексу");
-                        Console.WriteLine("5. Поиск элементов в списке");
-                        Console.WriteLine("6. Копирование списка в другой список");
-                        Console.WriteLine("7. Вывод элементов списка");
+                        Console.WriteLine("Выбран список 1\n");
+                        DisplayOptionsForList(listInput);
+                        try
+                        {
+                            optionsInput = int.Parse(Console.ReadLine());
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("Ошибка ввода!");
+                            break;
+                        }
+
                         
-                       
-                        
+
+                        switch (optionsInput)
+                        {
+                            case 1:
+                                AddElementsToList(firstList);
+                                break;
+                            case 2:
+
+                                break;
+                            case 3:
+
+                                break;
+                            case 4:
+
+                                break;
+                            case 5:
+
+                                break;
+                            case 6:
+
+                                break;
+                            case 7:
+                                OutputElementsOfList(firstList);
+                                break;
+
+                            default:
+                                Console.Clear();
+                                Console.WriteLine("Такой операции нет!");
+                                break;
+                        }
                         break;
                     case 2:
                         Console.Clear();
-                        Console.WriteLine("Выбран список 2");
+                        Console.WriteLine("Выбран список 2\n");
+                        DisplayOptionsForList(listInput);
                         break;
                     default:
                         Console.Clear();
